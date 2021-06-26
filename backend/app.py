8f8ea85c468524e2cc705e5c0841d60477bf2e0a
Alejandro Gallego <camgrafiman@gmail.com>
2021-06-26 19:31:26 +0200

    added build frontend folder

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
 
-app = Flask(__name__, static_folder="../frontend/build", static_url_path="/")
+app = Flask(__name__, static_folder="./build", static_url_path="/")
 
 CORS(app, resources={r"/*": {"origins": "*"}})
 
