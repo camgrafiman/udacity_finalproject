2e74d95f95766e55d469de44e54f699144e6b94a
Alejandro Gallego <camgrafiman@gmail.com>
2021-07-01 00:05:22 +0200

    added backend routes

--- backend/app.py
+++ backend/app.py
@@ -3,9 +3,14 @@ from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import random
+import datetime
+
+from database.models import setup_db, db_drop_and_create_all, User, Category, Character, Show
+from auth.auth import AuthError, requires_auth
+
 
 app = Flask(__name__, static_folder="./build", static_url_path="/")
-
+setup_db(app)
 CORS(app, resources={r"/*": {"origins": "*"}})
 
 # CORS Headers:
@@ -20,11 +25,12 @@ def after_request(response):
     return response
 
 
+# Main route to serve frontend:
 @app.route("/")
 def index():
     return app.send_static_file('index.html')
 
-
+# API MAIN ENDPOINT:
 @app.route("/api/")
 def apiMain():
     return jsonify({
@@ -32,6 +38,427 @@ def apiMain():
     })
 
 
-# Iniciar la aplicación:
+@app.route("/api/user")
+def apiUsers():
+    users = User.query.all()
+    return jsonify({
+        "users": [user.format() for user in users],
+        "success": True
+    })
+
+
+'''
+[# CATEGORY ENDPOINTS #]
+
+'''
+
+
+@app.route("/api/categories", methods=['GET'])
+def categories():
+
+    try:
+        categories = Category.query.order_by(Category.id).all()
+
+        return jsonify({
+            "success": True,
+            "categories": [category.format() for category in categories]
+        })
+    except:
+        abort(422)
+
+
+@app.route("/api/categories", methods=['POST'])
+def categories_post():
+    request_data = request.get_json()
+
+    try:
+        if 'cat_type' not in request_data or 'cat_type' == '':
+            abort(422)
+
+        category_type = request_data['cat_type']
+        new_category = Category(cat_type=category_type)
+        new_category.insert()
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'category': new_category.format()
+        })
+
+    except:
+        abort(422)
+
+
+@app.route("/api/categories/<int:category_id>", methods=['PATCH'])
+def category_patch(category_id):
+
+    category = Category.query.get_or_404(category_id)
+    try:
+        request_data = request.get_json()
+        category.cat_type = request_data['cat_type']
+        category.update()
+
+        return jsonify({
+            "success": True,
+            "category": category.format()
+        })
+    except:
+        abort(422)
+
+
+@app.route("/api/categories/<int:category_id>", methods=['DELETE'])
+def category_delete(category_id):
+
+    category = Category.query.get_or_404(category_id)
+    try:
+        category.delete()
+
+        return jsonify({
+            "success": True,
+            "category_deleted": category.format()
+        })
+    except:
+        abort(422)
+
+
+'''
+[# CHARACTER ENDPOINTS #]
+
+'''
+
+
+@app.route("/api/characters", methods=['GET'])
+def characters():
+    characters = Character.query.order_by(Character.id).all()
+    return jsonify({
+        'success': True,
+        'status_code': 200,
+        'characters': [character.format() for character in characters]
+
+    })
+
+
+@app.route("/api/characters/<int:character_id>", methods=['GET'])
+def character_get(character_id):
+
+    try:
+        character_data = Character.query.get_or_404(character_id)
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'character': character_data.format(),
+            'character_full': character_data.formatFull()
+        })
+
+    except:
+        abort(404)
+
+
+@app.route("/api/characters", methods=['POST'])
+def characters_post():
+
+    try:
+        request_data = request.get_json()
+        if 'name' not in request_data or 'character_name' not in request_data:
+            abort(422)
+
+        named = request_data['name']
+        character_named = request_data['character_name']
+        aged = request_data['age']
+        genderd = request_data['gender']
+        imaged = request_data['image']
+
+        new_character = Character(
+            name=named, character_name=character_named, age=aged, gender=genderd, image=imaged)
+        print(new_character)
+        new_character.insert()
+
+        # casts = Show.query.filter(Show.name.in_(data['showcasts'])).all()
+        # casts = data['showcasts']
+        # print(casts)
+        # new_character.showcasts = casts
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'character': new_character.format()
+        })
+
+    except:
+        abort(422)
+
+
+@app.route("/api/characters/<int:character_id>", methods=['PATCH'])
+def character_patch(character_id):
+    character_data = Character.query.get_or_404(character_id)
+
+    try:
+
+        request_data = request.get_json()
+        print(request_data)
+
+        if 'name' in request_data:
+            if request_data['name'] == '':
+                abort(422)
+            character_data.name = request_data['name']
+        if 'character_name' in request_data:
+            if request_data['character_name'] == '':
+                abort(422)
+            character_data.character_name = request_data['character_name']
+        if 'age' in request_data:
+            if request_data['age'] == 0:
+                abort(422)
+            character_data.age = request_data['age']
+        if 'gender' in request_data:
+            if request_data['gender'] == '':
+                abort(422)
+            character_data.gender = request_data['gender']
+
+        if 'image' in request_data:
+            if request_data['image'] == '':
+                abort(422)
+
+            character_data.image = request_data['image']
+
+        character_data.update()
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'character': character_data.format()
+        })
+    except:
+        abort(422)
+
+
+@app.route("/api/characters/<int:character_id>", methods=['DELETE'])
+def character_delete(character_id):
+    character_data = Character.query.get_or_404(character_id)
+
+    try:
+        character_data.delete()
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'character_deleted': character_data.format()
+
+        })
+    except:
+        abort(422)
+
+
+#
+
+'''
+[# SHOW ENDPOINTS #]
+
+'''
+@app.route("/api/shows", methods=['GET'])
+def shows():
+    shows = Show.query.all()
+    return jsonify({
+        'success': True,
+        'status_code': 200,
+        'shows': [show.format() for show in shows]
+    })
+
+
+@app.route("/api/shows/<int:show_id>", methods=['GET'])
+def show_get(show_id):
+    show = Show.query.get_or_404(show_id)
+    try:
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'show': show.formatFull()
+        })
+    except:
+        abort(422)
+
+
+@app.route("/api/shows", methods=['POST'])
+def shows_post():
+    request_data = request.get_json()
+    try:
+
+        if 'title' not in request_data or 'show_type' not in request_data or 'show_description' not in request_data or 'release_date' not in request_data or 'rating' not in request_data:
+            abort(422)
+
+        if request_data['title'] == '' or request_data['show_type'] == '' or request_data['release_date'] <= 0 or request_data['rating'] < 0:
+            abort(422)
+
+        title = request_data['title']
+        show_type = request_data['show_type']
+        show_description = request_data['show_description']
+        release_date = request_data['release_date']
+        rating = request_data['rating']
+
+        new_show = Show(title=title, show_type=show_type,
+                        show_description=show_description, release_date=release_date, rating=rating)
+        new_show.insert()
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'show': new_show.format(),
+            'showFull': new_show.formatFull()
+        })
+
+    except:
+        abort(422)
+
+
+@app.route("/api/shows/<int:show_id>", methods=['PATCH'])
+def shows_patch(show_id):
+    show_data = Show.query.get_or_404(show_id)
+    try:
+        request_data = request.get_json()
+        if 'title' not in request_data or 'show_type' not in request_data or 'show_description' not in request_data or 'release_date' not in request_data or 'rating' not in request_data:
+            abort(422)
+
+        if request_data['title'] == '' or request_data['show_type'] == '' or request_data['release_date'] <= 0 or request_data['rating'] < 0:
+            abort(422)
+        if request_data['release_date'] <= 0:
+            abort(422)
+        if request_data['rating'] == '' or 'rating' not in request_data:
+            abort(422)
+
+        show_data.title = request_data['title']
+        show_data.show_type = request_data['show_type']
+        show_data.show_description = request_data['show_description']
+        show_data.release_date = request_data['release_date']
+        show_data.rating = request_data['rating']
+
+        show_data.update()
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'showFull': show_data.formatFull()
+        })
+
+    except:
+        abort(422)
+
+
+@app.route("/api/shows/<int:show_id>", methods=['DELETE'])
+def shows_delete(show_id):
+    show_data = Show.query.get_or_404(show_id)
+    try:
+        show_data.delete()
+
+        return jsonify({
+            'success': True,
+            'status_code': 200,
+            'show_deleted': show_data.format()
+        })
+
+    except:
+        abort(422)
+
+
+# TESTING: Used to erase data from the database tables and create a testUser.
+# db_drop_and_create_all()
+''' 
+[ERROR HANDLERS]
+'''
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'status_code': 400,
+        'message': 'Bad request'
+    }), 400
+
+
+@app.errorhandler(401)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 401,
+        'message': 'Sorry, you are not authorized/allowed to view this content.'
+    }), 401
+
+
+@app.errorhandler(403)
+def forbidden(error):
+    return jsonify({
+        'success': False,
+        'error': 403,
+        'message': 'Forbidden'
+    }), 403
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Not found.'
+    }), 404
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        'success': False,
+        'status_code': 422,
+        'message': 'unprocessable'
+    }), 422
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    return jsonify({
+        'success': False,
+        'status_code': 500,
+        'message': 'Internal server error.'
+    }), 500
+
+
+@app.errorhandler(502)
+def bad_gateway(error):
+    return jsonify({
+        'success': False,
+        'status_code': 502,
+        'message': 'Bad gateway.'
+    }), 502
+
+
+@app.errorhandler(503)
+def service_unavailable(error):
+    return jsonify({
+        'success': False,
+        'status_code': 503,
+        'message': 'Service unavailable'
+    }), 503
+
+
+@app.errorhandler(504)
+def gateway_out(error):
+    return jsonify({
+        'success': False,
+        'error': 504,
+        'message': 'Gateway timed out'
+    }), 504
+
+
+'''
+Error handler for AuthError
+'''
+# Error Handler (401-403) Unauthorized
+@app.errorhandler(AuthError)
+def not_authenticated(auth_error):
+    return jsonify({
+        "success": False,
+        "error": auth_error.status_code,
+        "message": auth_error.error
+    }), auth_error.status_code
+
+
+# Init the application:
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ backend/database/models.py
@@ -0,0 +1,243 @@
+import os
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, DateTime, create_engine
+from sqlalchemy.sql import func
+from flask_sqlalchemy import SQLAlchemy
+import json
+import datetime
+# environment variables using python-decouple (.env) file :
+from decouple import config
+
+# DATABASE_NAME = config('DATABASE_NAME')
+# DATABASE_USER = config('DATABASE_USER')
+
+DATABASE_URI = config('DATABASE_URI_LOCAL')
+print(DATABASE_URI)
+
+db = SQLAlchemy()
+
+'''
+setup_db(app) binds a flask application and a SQLAlchemy service
+'''
+
+
+def setup_db(app, database_path=DATABASE_URI):
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+    print("DATABASE SETUP COMPLETE.")
+
+
+class User(db.Model):
+    __tablename__ = 'test'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=80))
+    password = Column(String(length=80))
+
+    def __init__(self, name, password):
+        self.name = name
+        self.password = password
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'password': self.password
+        }
+
+
+# Association table Many-to-Many between Character and Show Classes/tables.
+casts = db.Table('casts', db.Column('character_id', db.Integer, db.ForeignKey(
+    'characters.id'), primary_key=True), db.Column('show_id', db.Integer, db.ForeignKey('shows.id'), primary_key=True))
+
+
+class Character(db.Model):
+    __tablename__ = 'characters'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(length=150), nullable=False)
+    character_name = Column(String(length=150), nullable=False)
+    age = Column(Integer, default=0)
+    gender = Column(String(length=50))
+    image = Column(
+        String, default="https://www.labicok.com/wp-content/uploads/2020/09/default-user-image.png")
+    showcasts = db.relationship(
+        'Show', secondary=casts, backref=db.backref('cast', lazy=True))
+
+    def __init__(self, name, character_name, age, gender, image):
+        self.name = name
+        self.character_name = character_name
+        self.age = age,
+        self.gender = gender,
+        self.image = image
+
+    def __repr__(self):
+        return f'<<Character Name: {self.name} - age: {self.age} - gender: {self.gender} - img: {self.image} >>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def characterShows(character_id):
+        return Show.query.filter(Show.character_id == character_id).all()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'character_name': self.character_name,
+            'gender': self.gender
+        }
+
+    def formatFull(self):
+
+        return {
+            'id': self.id,
+            'name': self.name,
+            'age': self.age,
+            'character_name': self.character_name,
+            'gender': self.gender,
+            'image': self.image,
+            'shows': [{
+                'id': show.id,
+                'title': show.title,
+                'show_type': show.show_type,
+                'release_date': show.release_date,
+                'rating': show.rating
+            } for show in self.showcasts]
+        }
+
+
+class Show(db.Model):
+    __tablename__ = 'shows'
+    id = Column(Integer, primary_key=True)
+    title = Column(String, unique=True, nullable=False)
+    show_type = Column(String)  # series, movie, trailer
+    show_description = Column(String)
+    release_date = Column(Integer, nullable=False)
+    rating = Column(Float, nullable=False, default=0.0)
+    categories = db.relationship('Category', backref='show')
+
+    # Show.cast  (self.cast) is available.
+
+    def __init__(self, title, show_type, show_description, release_date, rating):
+        self.title = title
+        self.show_type = show_type
+        self.show_description = show_description
+        self.release_date = release_date
+        self.rating = rating
+
+    def __repr__(self):
+        return f'<Show {self.id} - {self.title} - {self.categories[0].cat_type}'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'show_type': self.show_type
+
+        }
+
+    def formatFull(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'show_type': self.show_type,
+            'show_description': self.show_description,
+            'release_date': self.release_date,
+            'rating': self.rating,
+            'cast': [character.format() for character in self.cast],
+            'categories': [category.cat_type for category in self.categories]
+        }
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+
+    id = Column(Integer, primary_key=True)
+    cat_type = Column(String)
+    show_id = Column(Integer, db.ForeignKey('shows.id'))
+
+    # def __init__(self, cat_type):
+    #     self.cat_type = cat_type
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'cat_type': self.cat_type
+        }
+
+
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to have multiple verisons of a database
+    COMMENT TO DEACTIVATE:
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    # add one demo row which is helping in POSTMAN test:
+    user = User(
+        name='testUser',
+        password='testPass'
+    )
+
+    user.insert()
+
+    # Add default categories and default No-Show:
+    noShow = Show(title='No show', show_type='No type',
+                  show_description='No show', release_date=2021, rating=0.0)
+    noShow.insert()
+
+    categoryUnkwnown = Category(cat_type='Unknown', show=noShow)
+    categoryMovies = Category(cat_type='Movies', show=noShow)
+    categorySeries = Category(cat_type='Series', show=noShow)
+    categoryTrailer = Category(cat_type='Trailer', show=noShow)
+
+    categoryUnkwnown.insert()
+    categoryMovies.insert()
+    categorySeries.insert()
+    categoryTrailer.insert()
